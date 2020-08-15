e40f3816f8db74ed77c04bcc36812e76c50b909e
Benjamin Bouvier <public@benj.me>
2020-08-15 17:45:02 +0200

    docs

---
+++ README.md
@@ -0,0 +1,8 @@
+nextcloud_analytics_rs
+===
+
+A small Rust wrapper to call the [Nextcloud Analytics
+API](https://github.com/rello/analytics/wiki/API), for databases of type
+"internal database".
+
+See examples of usage on docs.rs or src/lib.rs.
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
+//! # nextcloud_analytics_rs
+//!
+//! A small Rust wrapper to call the [Nextcloud Analytics
+//! API](https://github.com/rello/analytics/wiki/API), for databases of type "internal database".
+//!
+//! Example of usage:
+//!
+//! ```
+//!   let base_url = "https://example.com/nextcloud";
+//!   let collection = 42;
+//!   let user = "myself";
+//!   let passwd = "hunter2";
+//!
+//!   let client = nextcloud_analytics_rs::SyncClient::new(base_url, collection, user, passwd);
+//!   client.send_timeline_now_data("speed_kmh", 180).unwrap_or_else(|_| println!("api or network error"));
+//!   client.send_timeline_now_data("power_level", 9001).unwrap_or_else(|_| println!("api or network error"));
+//!
+//!   let other_collection = 3;
+//!   let client = nextcloud_analytics_rs::SyncClient::new(base_url, other_collection, user, passwd);
+//!   client.send_data("age", "alice", 25).unwrap_or_else(|_| println!("api or network error"));
+//!   client.send_data("age", "bob", 20).unwrap_or_else(|_| println!("api or network error"));
+//! ```
+
 use core::fmt;
 use std::error::Error;
 
@@ -6,6 +29,7 @@ use reqwest as http;
 
 static URL_PREFIX: &'static str = "apps/analytics/api/1.0/adddata/{COLLECTION_ID}";
 
+/// A synchronous client to call the Nextcloud Analytics API.
 pub struct SyncClient {
     client: http::blocking::Client,
     url: String,
@@ -14,6 +38,13 @@ pub struct SyncClient {
 }
 
 impl SyncClient {
+    /// Create a new synchronous client to call the Nextcloud Analytics API.
+    ///
+    /// - `nextcloud_url` is the base URL of the Nextcloud instance.
+    /// - `collection` is the collection index, as presented by Nextcloud Analytics' interface
+    /// (number in the URL).
+    /// - `user` is the Nextcloud user's name.
+    /// - `passwd` is an app password associaetd to the Nextcloud user's account.
     pub fn new<S: Into<String>>(nextcloud_url: &str, collection: u32, user: S, passwd: S) -> Self {
         let mut url = nextcloud_url.to_string();
 
@@ -44,6 +75,10 @@ impl SyncClient {
         }
     }
 
+    /// Sends some data to the API, the two first dimensions must be formatted as text while the
+    /// last dimension must be a numerical value.
+    ///
+    /// For timeline data, `dimension2` must be the date in the RFC2822 format.
     pub fn send_data<S: Into<String>, F: Into<f64>>(
         &self,
         dimension1: S,
@@ -93,6 +128,8 @@ impl SyncClient {
         Ok(())
     }
 
+    /// Sends some timeline data to the API: the `key` is the index of this piece of data,
+    /// associated to the given `value` at the given `time`. for the given `time`.
     pub fn send_timeline_data<S: Into<String>, F: Into<f64>>(
         &self,
         key: S,
@@ -102,6 +139,8 @@ impl SyncClient {
         self.send_data(key.into(), time.to_rfc2822(), value.into())
     }
 
+    /// Sends some timeline data to the API: the `key` is the index of this piece of data,
+    /// associated to the given `value` at the current UTC time.
     pub fn send_timeline_now_data<S: Into<String>, F: Into<f64>>(
         &self,
         key: S,
@@ -111,6 +150,7 @@ impl SyncClient {
     }
 }
 
+/// A simple error wrapper for API errors.
 pub struct ApiError(String);
 
 impl fmt::Debug for ApiError {
